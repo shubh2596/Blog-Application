a9ecfb33316ae6dc6e034614fecca12114a71d1b
Shubham Jain <shubhamjainy1996@gmailcom>
2023-12-17 21:03:40 +0530

    These are my code snippets of react .

---
+++ 01viteReact/src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// function Element (){
+//   return (
+//     <h1>i am a new elemen | </h1>
+//   )
+// }
+
+// const reactElement = {
+//   type : 'a',
+//   props : {
+//       href: 'http://google.com',
+//       target: '_blank'
+//   } ,
+//   children : 'Click me to visit google '
+// };
+
+// const anotherUser = "xyz"
+// const reactElement = React.createElement('a',
+// {href:'http://google.com',
+// target:'_blank'},
+// 'click me to visit google',
+// anotherUser
+// )
+
+
+ReactDOM.createRoot(
+  
+    // <Element ></Element>document.getElementById('root')).render(
+    <React.StrictMode>
+     <App ></App>
+    </React.StrictMode>
+    
+  
+    
+   
+    
+)
+ 
